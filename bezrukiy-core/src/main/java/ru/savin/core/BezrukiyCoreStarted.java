fe8d1a503a380606ce46534ba32a4cbb95015eb9
Dmitry Savin <153686845+dmtrsavin@users.noreply.github.com>
2024-02-25 15:12:53 +0300

    Внесены небольшие изменения в конфигурацию + добавлен uereka-server (#4)

--- bezrukiy-core/src/main/java/ru/savin/core/BezrukiyCoreStarted.java
+++ bezrukiy-core/src/main/java/ru/savin/core/BezrukiyCoreStarted.java
@@ -2,13 +2,12 @@ package ru.savin.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * Запуск приложения "Безрукий".
  */
+@RefreshScope
 @SpringBootApplication
 public class BezrukiyCoreStarted {
     public static void main(String[] args) {
--- bezrukiy-files/src/main/java/ru/savin/files/BezrukiyFilesStarted.java
+++ bezrukiy-files/src/main/java/ru/savin/files/BezrukiyFilesStarted.java
@@ -2,10 +2,12 @@ package ru.savin.files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * Запуск приложения файловой составляющей.
  */
+@RefreshScope
 @SpringBootApplication
 public class BezrukiyFilesStarted {
     public static void main(String[] args) {
